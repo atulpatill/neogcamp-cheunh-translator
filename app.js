46b34be8bd84b21509476f99de6b5d5815a81c09
Atul Patil <atulpatil539@gmail.com>
2021-04-16 18:58:13 +0530

    final done

---
+++ app.js
@@ -0,0 +1,35 @@
+var btn = document.querySelector("#clickme");
+var txtIn = document.querySelector("#userinput");
+var output = document.querySelector("#output");
+
+var serverURL = "https://api.funtranslations.com/translate/cheunh.json";
+
+function translateURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler(error) {
+  console.log("error occured", error);
+  alert("something wrong with server! try again after some time");
+}
+
+// function btnHandler() {
+//     var inputText = txtInput.value; // taking input
+
+//     // calling server for processing
+//     fetch(getTranslationURL(inputText))
+//         .then(response => response.json())
+//         .then(json => {
+//             var translatedText = json.contents.translated;
+//             divOut.innerText = translatedText; // output
+//            })
+//         .catch(errorHandler)
+// };
+
+// btnTranslate.addEventListener("click", btnHandler)
+
+btn.addEventListener("click", function btnHandler() {
+  fetch(translateURL(userinput.value))
+    .then((response) => response.json())
+    .then((json) => {
+      output.innerText = json.contents.translated
