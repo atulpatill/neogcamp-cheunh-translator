5857be76049c208019c18715e6d0c60db6613e57
Atul Patil <atulpatil539@gmail.com>
2021-08-24 12:53:34 +0530

    fix:updated

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var btn = document.querySelector("#clickme");
 var txtIn = document.querySelector("#userinput");
 var output = document.querySelector("#output");
 
-// var serverURL = "https://api.funtranslations.com/translate/cheunh.json";
-var serverURL = "https://api.funtranslations.com/translate/minion.json";
+var serverURL = "https://api.funtranslations.com/translate/cheunh.json";
+// var serverURL = "https://api.funtranslations.com/translate/minion.json";
 
 function translateURL(text) {
   return serverURL + "?" + "text=" + text;
@@ -14,23 +14,12 @@ function errorHandler(error) {
   alert("something wrong with server! try again after some time");
 }
 
-// function btnHandler() {
-//     var inputText = txtInput.value; // taking input
-
-//     // calling server for processing
-//     fetch(getTranslationURL(inputText))
-//         .then(response => response.json())
-//         .then(json => {
-//             var translatedText = json.contents.translated;
-//             divOut.innerText = translatedText; // output
-//            })
-//         .catch(errorHandler)
-// };
-
-// btnTranslate.addEventListener("click", btnHandler)
 
 btn.addEventListener("click", function btnHandler() {
   fetch(translateURL(userinput.value))
     .then((response) => response.json())
     .then((json) => {
-      output.innerText = json.contents.translated
+      output.innerText = json.contents.translated;
+    })
+    .catch(errorHandler);
+});
